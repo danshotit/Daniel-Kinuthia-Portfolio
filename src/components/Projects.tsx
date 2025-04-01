c0a7baa1459196439d24f90bbf3d30b0871b0bb3
Daniel Kinuthia <danshotit91@gmail.com>
2025-04-01 21:02:07 +0300

    github link 2

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -141,7 +141,7 @@ const Projects = () => {
                     <ArrowUpRight size={14} />
                   </a>
                   <a 
-                    href={project.githubUrl}
+                    href="https://github.com/danshotit"
                     className="bg-white/20 text-white px-4 py-2 rounded-full text-sm font-medium hover:bg-white/30 transition-colors"
                   >
                     Code
